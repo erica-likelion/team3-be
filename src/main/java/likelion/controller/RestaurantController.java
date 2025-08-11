7a56a27ce9855e946ec9db8690832a66a43e6d58
kyoohyunlee <khyhlee@naver.com>
2025-08-11 20:13:08 +0900

    반경 계산 테스트 용(추후 수정 가능)

---
+++ src/main/java/likelion/controller/RestaurantController.java
@@ -0,0 +1,29 @@
+package likelion.controller;
+
+import likelion.domain.entity.Restaurant;
+import likelion.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class RestaurantController {
+    /**
+     * 위도경도 정보만 받아서
+     * 해당 인근 150미터 지점 음식점 리스트 반환하는
+     * 테스트 용이에요. 수정하셔서 쓰시면 됩니다.
+     * 새로 만드셔도 됩니다. 편하신대로!
+     */
+    @Autowired
+    private RestaurantService restaurantService;
+
+    @GetMapping("analysis")
+    public List<Restaurant> getRestaurantsWithinRadius(
+            @RequestParam double latitude,
+            @RequestParam double longitude) {
+        return restaurantService.getRestaurantsWithinRadius(latitude, longitude);
+    }
+}
