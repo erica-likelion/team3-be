88cfc782d92352b45ae4e62570d4762e92c54355
kyoohyunlee <khyhlee@naver.com>
2025-08-13 00:21:42 +0900

    feat: 인근 음식점 아이디로 리뷰 찾아오기

--- src/main/java/likelion/repository/ReviewRepository.java
+++ src/main/java/likelion/repository/ReviewRepository.java
@@ -2,10 +2,20 @@ package likelion.repository;
 
 import likelion.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    Optional<Review> findByRestaurant_KakaoPlaceIdAndSourceReviewId(Long placeId, String sourceId);
+    @Query("""
+        select v
+        from Review v
+        where v.restaurant.kakaoPlaceId in :placeIds
+        order by v.id desc
+    """)
+    List<Review> findAllByRestaurantKakaoPlaceIdInOrderByIdDesc(@Param("placeIds") Collection<Long> placeIds);
 }
 
