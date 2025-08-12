e16cfef3bc95277bef8ed1b75a736997b108a808
kyoohyunlee <khyhlee@naver.com>
2025-08-12 20:59:21 +0900

    fix: 레포, 서비스 진수님 의견대로 개선

--- src/main/java/likelion/repository/RestaurantRepository.java
+++ src/main/java/likelion/repository/RestaurantRepository.java
@@ -2,10 +2,21 @@ package likelion.repository;
 
 import likelion.domain.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     /**
      * 인터페이스
      */
+    @Query(value = """
+        SELECT * FROM restaurant r
+        WHERE ST_Distance_Sphere(POINT(r.longitude, r.latitude), POINT(:longitude, :latitude)) <= :radius
+        """, nativeQuery = true)
+    List<Restaurant> findRestaurantsWithinRadius(@Param("latitude") double latitude,
+                                                 @Param("longitude") double longitude,
+                                                 @Param("radius") int radius);
 }
