f42213dc9e1a1b72980f275ebf8885da055c053c
kyoohyunlee <khyhlee@naver.com>
2025-08-19 19:37:30 +0900

    feat: exception 처리

--- src/main/java/likelion/controller/AnalysisController.java
+++ src/main/java/likelion/controller/AnalysisController.java
@@ -1,6 +1,7 @@
 package likelion.controller;
 
 
+import jakarta.validation.Valid;
 import likelion.controller.dto.AnalysisRequest;
 import likelion.controller.dto.AnalysisResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import likelion.service.AnalysisService;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +19,10 @@ public class AnalysisController {
     private final AnalysisService analysisService;
 
     @PostMapping("/api/analysis")
-    public AnalysisResponse getAnalysis(@RequestBody AnalysisRequest request) {
+    public AnalysisResponse getAnalysis(@Valid @RequestBody AnalysisRequest request) {
+        if (request == null){
+            throw new ResponseStatusException(BAD_REQUEST, "요청이 비어있음");
+        }
         return analysisService.analyze(request);
     }
 
