a2f889e6e7f15a5ae37e968f376296d96a7df2c4
kyoohyunlee <khyhlee@naver.com>
2025-08-22 04:11:04 +0900

    feat: dto 생성

--- src/main/java/likelion/dto/PostCreateRequestDto.java
+++ src/main/java/likelion/dto/PostCreateRequestDto.java
@@ -4,18 +4,19 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import likelion.domain.entity.Category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Schema(description = "게시글 작성 정보")
 @Getter
+@Setter
 @NoArgsConstructor
-public class PostRequestDto {
+public class PostCreateRequestDto {
 
     private String title;
     private String content;
-    private MultipartFile image;
     private String userName;
-    private String password;
+
     @Schema(description = "GENERAL or PARTNERSHIP")
     private Category category; //자유 or 제휴
 }
--- src/main/java/likelion/dto/PostResponseDto.java
+++ src/main/java/likelion/dto/PostResponseDto.java
@@ -1,4 +1,28 @@
 package likelion.dto;
 
+import likelion.domain.entity.Category;
+import likelion.domain.entity.Post;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
 public class PostResponseDto {
+    private final Long id;
+    private final String userName;
+    private final String title;
+    private final String content;
+    private final LocalDateTime createdAt;
+    private final Category category;
+    private final String imageUrl;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.userName = post.getUserName();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
+        this.category = post.getCategory();
+        this.imageUrl = post.getImageUrl();
+    }
 }
