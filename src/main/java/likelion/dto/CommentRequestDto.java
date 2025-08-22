184ca60f10b0a24425051cc5bd016f906182207d
임진수 <jsbd0109@naver.com>
2025-08-23 02:50:34 +0900

    feat: CommentResponse/RequestDto 생성

--- src/main/java/likelion/dto/CommentRequestDto.java
+++ src/main/java/likelion/dto/CommentRequestDto.java
@@ -1,4 +1,16 @@
 package likelion.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Schema(description = "댓글 작성 요청")
+@Getter @Setter @NoArgsConstructor
 public class CommentRequestDto {
+
+    @Schema(description = "댓글 내용", example = "댓글내용 댓글내용 예시 예시")
+    @NotBlank(message = "content는 필수입니다.")
+    private String content;
 }
--- src/main/java/likelion/dto/CommentResponseDto.java
+++ src/main/java/likelion/dto/CommentResponseDto.java
@@ -1,4 +1,25 @@
 package likelion.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import likelion.domain.entity.Comment;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Schema(description = "댓글 응답")
+@Getter
 public class CommentResponseDto {
+    private final Long id;
+    private final Long postId;
+    private final String userName;
+    private final String content;
+    private final LocalDateTime createdAt;
+
+    public CommentResponseDto(Comment c) {
+        this.id = c.getId();
+        this.postId = (c.getPost() != null ? c.getPost().getId() : null);
+        this.userName = c.getUserName(); // 익명 디폴트
+        this.content = c.getContent();
+        this.createdAt = c.getCreated_at();
+    }
 }
