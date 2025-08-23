0a1432080394e664032e019ebd162e27e4396261
kyoohyunlee <khyhlee@naver.com>
2025-08-23 19:10:11 +0900

    feat: findRestaurantByName 추가

--- src/main/java/likelion/repository/RestaurantRepository.java
+++ src/main/java/likelion/repository/RestaurantRepository.java
@@ -3,7 +3,10 @@ package likelion.repository;
 import likelion.domain.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
+    Optional<Restaurant> findByRestaurantName(String restaurantName);
 }
