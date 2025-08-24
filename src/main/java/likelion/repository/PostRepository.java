255b4ebb2a1892d57fc279aad67189c097219434
kyoohyunlee <khyhlee@naver.com>
2025-08-25 05:16:05 +0900

    feat: 댓글 수 대로 게시글 출력

--- src/main/java/likelion/repository/PostRepository.java
+++ src/main/java/likelion/repository/PostRepository.java
@@ -2,6 +2,8 @@ package likelion.repository;
 
 import likelion.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByCommentCountDescIdDesc();
 }
